1b2d90bb5e83f684236aeae33421a6abbe8c0dfd
Surya Makthal <smaktha@ncsu.edu>
2019-09-15 01:40:31 -0400

    Completes authorization process with authorization code flow

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -1,17 +1,40 @@
 package auth
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/pkg/errors"
 )
 
+type Token struct {
+	AccessToken  string `json:"access_token"`
+	TokenType    string `json:"token_type"`
+	Scope        string `json:"scope"`
+	ExpiresIn    int    `json:"expires_in"`
+	RefreshToken string `json:"refresh_token"`
+}
+
 type Auth struct {
 	clientID     string
 	clientSecret string
+	Token        *Token
+}
+
+func (a *Auth) GetClientID() string {
+
+	return a.clientID
+
+}
+
+func (a *Auth) GetClientSecret() string {
+
+	return a.clientSecret
+
 }
 
 func (a *Auth) ExtractAppRegistration() error {
@@ -33,17 +56,24 @@ func (a *Auth) ExtractAppRegistration() error {
 
 }
 
-func (a *Auth) RequestAuthorization() ([]byte, error) {
+func (a *Auth) RequestToken(code string) ([]byte, error) {
 
-	resp, err := http.Get("https://accounts.spotify.com/authorize")
+	resp, err := http.PostForm("https://accounts.spotify.com/api/token",
+		url.Values{"client_id": {a.clientID}, "client_secret": {a.clientSecret}, "grant_type": {"authorization_code"}, "code": {code}, "redirect_uri": {"http://localhost:8080/callback"}})
 	if err != nil {
-		return nil, errors.Wrap(err, "[OOF-AUTH] Error sending request to Spotify Accounts Service")
+		return nil, errors.Wrap(err, "[OOF-AUTH] Error sending token request")
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, errors.Wrap(err, "[OOF-AUTH] Error reading response from Spotify Accounts Service")
+		return nil, errors.Wrap(err, "[OOF-AUTH] Error reading token response")
 	}
+	token := &Token{}
+	err = json.Unmarshal(body, token)
+	if err != nil {
+		return nil, errors.Wrap(err, "[OOF-AUTH] Error unmarshalling JSON")
+	}
+	a.Token = token
 	return body, nil
 
 }
